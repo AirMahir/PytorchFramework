b14756bcd8447905beaf9f8d0b7a88db332d8362
Mahir Jain <E87299@aiit.com>
2025-07-30 09:41:00 +0530

    ckeaning

--- trainers/classification_trainer.py
+++ trainers/classification_trainer.py
@@ -1,44 +1,48 @@
 import os
 import torch
+import logging
+import torch.nn as nn
 from tqdm.auto import tqdm
-from torch.cuda.amp import autocast, GradScaler 
-from utils.metrics import calculate_accuracy, calculate_dice_coefficient, calculate_iou_score
-from utils.visualize import display_segmentation_batch, display_segmentation_prediction, plot_metric_curves, plot_loss_curves
+from torch.cuda.amp import autocast, GradScaler
+from utils.visualize import display_classification_batch, display_classification_prediction, plot_loss_curves, plot_metric_curves
 
-class SegmentationTrainer:
+
+logger = logging.getLogger(__name__)
+
+class ClassificationTrainer(nn.Module):
 
     def __init__(self, model, train_loader, val_loader, optimizer, criterion, scheduler, device, config, logger=None):
 
-        self.model = model
+        super().__init__()
+
+        self.model = model 
         self.train_loader = train_loader
         self.val_loader = val_loader
         self.optimizer = optimizer
         self.criterion = criterion
+        self.scheduler = scheduler
         self.device = device
         self.config = config
-        self.logger = logger
+        self.logger = logger 
+        self.scaler = GradScaler() # Initialize GradScaler for mixed precision
 
-        self.scheduler = scheduler
-        self.scaler = GradScaler() 
+    def _train_one_epoch(self, epoch):
 
-    def _train_one_epoch(self, epoch, num_classes):
         self.model.train()
 
-        total_loss = 0
-        accuracy = []
-        iou_score = []
-        dice_coeff = []
+        train_loss = 0
+        train_correct_predictions = 0
 
-        for batch_idx, (images, masks) in enumerate(tqdm(self.train_loader, desc=f"Epoch {epoch+1} Train", leave=False)):
-            images, masks = images.to(self.device), masks.to(self.device)
-            masks = masks.long()
+        for batch_idx, (images, targets) in enumerate(tqdm(self.train_loader, desc=f"Epoch {epoch+1} Train")):
 
+            images, targets = images.to(self.device), targets.to(self.device).long()
+            
             # Autocast for automatic mixed precision
             with autocast():
-                preds = self.model(images)
-                loss = self.criterion(preds, masks)
+                targets_pred = self.model(images)
+                loss = self.criterion(targets_pred, targets)
             
-            total_loss += loss.item()
+            train_loss += loss.item()
 
             self.optimizer.zero_grad()
             
@@ -51,87 +55,78 @@ class SegmentationTrainer:
 
             if self.scheduler:
                 self.scheduler.step()
+    
+            target_pred_class = torch.argmax(targets_pred, dim = 1)
+            train_correct_predictions += (target_pred_class == targets).sum().item()
 
-            accuracy.append(calculate_accuracy(preds, masks))
-            iou_score.append(calculate_iou_score(preds, masks, num_classes=num_classes))
-            dice_coeff.append(calculate_dice_coefficient(preds, masks, num_classes=num_classes))
+        avg_train_loss = train_loss / len(self.train_loader)
+        avg_train_acc = train_correct_predictions / len(self.train_loader.dataset)
+        
+        self.logger.info(f"Epoch {epoch+1} Train - Avg Loss: {avg_train_loss:.4f}, Avg Acc: {avg_train_acc*100:.2f}%")
 
-        self.logger.info(f"Epoch {epoch+1} Train - Avg Loss: {total_loss / len(self.train_loader):.4f}, Avg Acc: {torch.tensor(accuracy).mean().item()*100:.2f}%, Avg IoU: {torch.tensor(iou_score).mean().item()*100:.2f}%, Avg Dice: {torch.tensor(dice_coeff).mean().item()*100:.2f}%")
+        return avg_train_acc, avg_train_loss
+    
+    def _val_one_epoch(self, epoch):
 
-        return (
-            total_loss / len(self.train_loader),
-            torch.tensor(accuracy).mean().item(),
-            torch.tensor(iou_score).mean().item(),
-            torch.tensor(dice_coeff).mean().item()
-        )
-
-    def _val_one_epoch(self, epoch, num_classes):
         self.model.eval()
 
-        total_loss = 0
-        accuracy = []
-        iou_score = []
-        dice_coeff = []
+        val_loss = 0
+        val_correct_predictions = 0
 
         with torch.no_grad():
-            for i, (images, masks) in enumerate(tqdm(self.val_loader, desc=f"Epoch {epoch+1} Val", leave=False)):
+            for i, (images, targets) in enumerate(tqdm(self.val_loader, desc=f"Epoch {epoch+1} Val", leave=False)):
 
                 if i == 0:
-                    display_segmentation_batch(images, masks, self.config)
+                    display_classification_batch(images, targets, self.config)
+                    
+                images, targets = images.to(self.device), targets.to(self.device).long()
 
-                images, masks = images.to(self.device), masks.to(self.device)
-                masks = masks.long()
-                
-                # No GradScaler needed in validation as there's no backward pass
+                # Autocast for automatic mixed precision during validation (no scaler needed)
                 with autocast():
-                    preds = self.model(images)
+                    test_preds = self.model(images)
 
-                if(epoch % 5 == 0):
-                    display_segmentation_prediction(images, masks, preds, epoch, self.config)
+                if epoch % 5 == 0:
+                    display_classification_prediction(images, targets, test_preds, epoch, self.config)
 
-                loss = self.criterion(preds, masks)
-                total_loss += loss.item()
+                loss = self.criterion(test_preds, targets)
+                val_loss += loss.item()
+                
+                test_pred_labels = torch.argmax(test_preds, dim=1)
+                val_correct_predictions += (test_pred_labels == targets).sum().item()
+            
+        avg_val_loss = val_loss / len(self.val_loader)
+        avg_val_acc = val_correct_predictions / len(self.val_loader.dataset)
 
-                accuracy.append(calculate_accuracy(preds, masks))
-                iou_score.append(calculate_iou_score(preds, masks, num_classes))
-                dice_coeff.append(calculate_dice_coefficient(preds, masks, num_classes))
+        self.logger.info(f"Epoch {epoch+1} Val - Avg Loss: {avg_val_loss:.4f}, Avg Acc: {avg_val_acc*100:.2f}%")
 
-            return (
-                total_loss / len(self.val_loader),
-                torch.tensor(accuracy).mean().item(),
-                torch.tensor(iou_score).mean().item(),
-                torch.tensor(dice_coeff).mean().item()
-            )
-        
+        return avg_val_acc, avg_val_loss
+    
     def train(self):
         num_epochs = self.config["num_epochs"]
-        results = {
-            "train_loss": [],
-            "train_acc": [],
-            "train_iou": [],
-            "train_dice": [],
-            "val_loss": [],
-            "val_acc": [],
-            "val_iou": [],
-            "val_dice": []
-        }
 
-        num_classes = self.config["num_classes"]
+        results = {"train_loss": [],
+                   "train_acc": [],
+                   "val_loss": [],
+                   "val_acc": []
+                }
         
         best_acc = 0
 
         self.logger.info("Starting training process...")
 
-        for epoch in tqdm(range(num_epochs), desc="Training Progress"):
-            train_loss, train_acc, train_iou, train_dice = self._train_one_epoch(epoch, num_classes)
-            val_loss, val_acc, val_iou, val_dice = self._val_one_epoch(epoch, num_classes)
+        for epoch in tqdm(range(num_epochs)):
+
+            train_acc, train_loss = self._train_one_epoch(epoch)
+            val_acc, val_loss = self._val_one_epoch(epoch)
 
             print(
-                f"Epoch {epoch+1:02d} | "
-                f"Train Loss: {train_loss:.4f} | Acc: {train_acc*100:.2f}% | IoU: {train_iou*100:.2f}% | Dice: {train_dice*100:.2f}% || "
-                f"Val Loss: {val_loss:.4f} | Acc: {val_acc*100:.2f}% | IoU: {val_iou*100:.2f}% | Dice: {val_dice*100:.2f}%"
+                f"Epoch: {epoch+1:02d} | "
+                f"train_loss: {train_loss:.4f} | "
+                f"train_acc: {train_acc*100:.2f}% | "
+                f"val_loss: {val_loss:.4f} | "
+                f"val_acc: {val_acc*100:.2f}%"
             )
-            self.logger.info(f"Epoch Summary: Epoch {epoch+1:02d} | Train Loss: {train_loss:.4f} | Acc: {train_acc*100:.2f}% | IoU: {train_iou*100:.2f}% | Dice: {train_dice*100:.2f}% || Val Loss: {val_loss:.4f} | Acc: {val_acc*100:.2f}% | IoU: {val_iou*100:.2f}% | Dice: {val_dice*100:.2f}%")
+            self.logger.info(f"Epoch Summary: Epoch: {epoch+1:02d} | train_loss: {train_loss:.4f} | train_acc: {train_acc*100:.2f}% | val_loss: {val_loss:.4f} | val_acc: {val_acc*100:.2f}%")
 
             if(val_acc > best_acc):
                 self.logger.info(f"Validation accuracy improved from {best_acc*100:.2f}% to {val_acc*100:.2f}%. Saving best model...")
@@ -145,19 +140,16 @@ class SegmentationTrainer:
                     'optimizer_state_dict' : self.optimizer.state_dict(),
                     'lr_scheduler_state_dict' : self.scheduler.state_dict(),
                     'scaler_state_dict': self.scaler.state_dict() # Save scaler state
-                } 
+                }
                 torch.save(checkpoint, model_path)
                 self.logger.info(f"Best model saved to {model_path}")
 
-            results["train_loss"].append(train_loss)
-            results["train_acc"].append(train_acc)
-            results["train_iou"].append(train_iou)
-            results["train_dice"].append(train_dice)
-            results["val_loss"].append(val_loss)
-            results["val_acc"].append(val_acc)
-            results["val_iou"].append(val_iou)
-            results["val_dice"].append(val_dice)
-
+            # 5. Update results dictionary
+            results["train_loss"].append(train_loss.item() if isinstance(train_loss, torch.Tensor) else train_loss)
+            results["train_acc"].append(train_acc.item() if isinstance(train_acc, torch.Tensor) else train_acc)
+            results["val_loss"].append(val_loss.item() if isinstance(val_loss, torch.Tensor) else val_loss)
+            results["val_acc"].append(val_acc.item() if isinstance(val_acc, torch.Tensor) else val_acc)
+        
         self.logger.info("Training process finished.")
         plot_loss_curves(results, self.config)
         plot_metric_curves(results, self.config)
